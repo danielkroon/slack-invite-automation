85ffec712d62d9334721d76e88cc2b7857dc913c
Daniel Kroon <drg.kroon@gmail.com>
2020-02-03 20:57:50 +0100

    deploy to heroku

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'YOUR-TEAM-NAME',
+  community: process.env.COMMUNITY_NAME || 'FitNesse Community',
   // your slack team url (ex: socketio.slack.com)
   slackUrl: process.env.SLACK_URL || 'YOUR-TEAM.slack.com',
   // access token of slack
